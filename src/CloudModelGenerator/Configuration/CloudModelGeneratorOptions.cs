2d5455603d52bd4549981dd85c5a34ce6a65dd09
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2017-10-24 12:15:50 +0100

    CloudModelGeneratorOptions file added

---
+++ src/CloudModelGenerator/Configuration/CloudModelGeneratorOptions.cs
@@ -0,0 +1,40 @@
+﻿namespace CloudModelGenerator
+{
+    public class CloudModelGeneratorOptions
+    {
+        /// <summary>
+        /// Kentico Cloud Project ID
+        /// </summary>
+        public string ProjectId { get; set; }
+
+        /// <summary>
+        /// Namespace name of the generated classes
+        /// </summary>
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Output directory for the generated files
+        /// </summary>
+        public string OutputDir { get; set; }
+
+        /// <summary>
+        /// Optionally add suffix to the generated files
+        /// </summary>
+        public string FilenameSuffix { get; set; }
+
+        /// <summary>
+        /// Optionally generate partial classes for user customisation
+        /// </summary>
+        public bool GeneratePartials { get; set; } = false;
+
+        /// <summary>
+        /// Indicates whether the CustomTypeProvider class should be generated
+        /// </summary>
+        public bool WithTypeProvider { get; set; } = true;
+
+        /// <summary>
+        /// Indicates whether the classes should be generated with types that represent structured data model
+        /// </summary>
+        public bool StructuredModel { get; set; } = false;
+    }
+}
