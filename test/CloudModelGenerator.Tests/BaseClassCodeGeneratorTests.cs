350b583efb4f4339ba1049460ba7dcaca43fd5a4
Simply007 <chrastina.ondra@gmail.com>
2018-07-09 13:51:09 +0200

    try to remove whitespace clearing and use equals method with ignoring white-spaces and line endings differences in tests

--- test/CloudModelGenerator.Tests/BaseClassCodeGeneratorTests.cs
+++ test/CloudModelGenerator.Tests/BaseClassCodeGeneratorTests.cs
@@ -20,10 +20,6 @@ namespace CloudModelGenerator.Tests
             string executingPath = AppContext.BaseDirectory;
             string expected_BaseClassCode = File.ReadAllText(executingPath + "/Assets/BaseClass_CompiledCode.txt");
 
-            // Line endings can vary due to git, so strip them out
-            expected_BaseClassCode = Regex.Replace(expected_BaseClassCode, @"\s+", "");
-            actualCompiled_BaseClass = Regex.Replace(actualCompiled_BaseClass, @"\s+", "");
-
             // Test base class
             Assert.Equal(actualCompiled_BaseClass, expected_BaseClassCode, ignoreLineEndingDifferences:true, ignoreWhiteSpaceDifferences: true);
         }
@@ -39,10 +35,6 @@ namespace CloudModelGenerator.Tests
             string actualCompiled_BaseClass = codeGenerator.GenerateBaseClassCode();
             string executingPath = AppContext.BaseDirectory;
             string expected_BaseClassCode = File.ReadAllText(executingPath + "/Assets/BaseClass_CompiledCode.txt");
-
-            // Line endings can vary due to git, so strip them out
-            actualCompiled_BaseClass = Regex.Replace(actualCompiled_BaseClass, @"\s+", "");
-            expected_BaseClassCode = Regex.Replace(expected_BaseClassCode, @"\s+", "");
             
             // Replace default namespace in expected result
             expected_BaseClassCode = expected_BaseClassCode.Replace(ClassCodeGenerator.DEFAULT_NAMESPACE, customNamespace);
@@ -61,10 +53,6 @@ namespace CloudModelGenerator.Tests
             string actualCompiled_ExtenderClass = codeGenerator.GenereateExtenderCode();
             string executingPath = AppContext.BaseDirectory;
             string expected_ExtenderCode = File.ReadAllText(executingPath + "/Assets/BaseClassExtender_CompiledCode.txt");
-
-            // Line endings can vary due to git, so strip them out
-            actualCompiled_ExtenderClass = Regex.Replace(actualCompiled_ExtenderClass, @"\s+", "");
-            expected_ExtenderCode = Regex.Replace(expected_ExtenderCode, @"\s+", "");
 
             // Test extender class
             Assert.Equal(actualCompiled_ExtenderClass, expected_ExtenderCode, ignoreLineEndingDifferences: true, ignoreWhiteSpaceDifferences: true);
