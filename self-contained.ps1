d9bf0b1db19e45a989648433013f01e6ba9b2319
Petr Švihlík <svihlik.petr@gmail.com>
2021-03-01 00:52:20 +0100

    switch to gh actions

---
+++ self-contained.ps1
@@ -0,0 +1,10 @@
+# Publish self-contained for Windows, Linux, and macOS
+
+$runtimes = "win-x64", "linux-x64", "osx-x64"
+
+Foreach($r in $runtimes)
+{
+	dotnet publish .\src\Kentico.Kontent.ModelGenerator\ -c Release --runtime $r -o (".\artifacts\" + $r) -p:PublishSingleFile=true -p:PublishTrimmed=true
+	Compress-Archive (".\artifacts\" + $r + "\*") (".\artifacts\KontentModelGenerator-" + $r + ".zip")
+	Remove-Item (".\artifacts\" + $r) -Recurse
+}
