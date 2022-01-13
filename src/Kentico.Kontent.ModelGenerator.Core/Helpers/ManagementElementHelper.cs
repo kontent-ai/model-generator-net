a1b027841c7c88a0c1154feee8da94b027ed3ae5
Martin Ševčík <sevcik.martin4@gmail.com>
2022-01-13 16:03:50 +0100

    122 subpages (#127)
    * 122 introduce subpages element + bump nuget
    
    * 122 introduce ManagementElementHelper
    
    * 122 introduce ElementTypeHelper
    
    * 122 subpages impl
    
    * 122 little refactoring
    
    * 122 little refactoring
    
    * 122 add missing test case

---
+++ src/Kentico.Kontent.ModelGenerator.Core/Helpers/ManagementElementHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Management.Models.Types;
+using Kentico.Kontent.Management.Models.Types.Elements;
+using Kentico.Kontent.Management.Models.TypeSnippets;
+
+namespace Kentico.Kontent.ModelGenerator.Core.Helpers
+{
+    public static class ManagementElementHelper
+    {
+        public static ElementMetadataBase GetManagementElement(
+            bool cmApi,
+            IContentElement deliverElement,
+            IEnumerable<ContentTypeSnippetModel> managementSnippets,
+            ContentTypeModel managementContentType)
+        {
+            if (!cmApi)
+            {
+                return null;
+            }
+
+            Validate(deliverElement, managementSnippets, managementContentType);
+
+            var managementContentTypeElement = managementContentType.Elements.FirstOrDefault(el => el.Codename == deliverElement.Codename);
+            if (managementContentTypeElement != null)
+            {
+                return managementContentTypeElement;
+            }
+
+            var managementSnippet = managementSnippets.FirstOrDefault(s =>
+                managementContentType.Elements.FirstOrDefault(el =>
+                    el.Type == ElementMetadataType.ContentTypeSnippet && el.Codename == s.Codename) != null);
+            if (managementSnippet == null)
+            {
+                throw new ArgumentException($"{nameof(managementSnippet)} shouldn't be null.");
+            }
+
+            var managementSnippetElement = managementSnippet.Elements.FirstOrDefault(el => el.Codename == deliverElement.Codename);
+            if (managementSnippetElement == null)
+            {
+                throw new ArgumentException($"{nameof(managementSnippetElement)} shouldn't be null.");
+            }
+
+            return managementSnippetElement;
+        }
+
+        private static void Validate(
+            IContentElement deliverElement,
+            IEnumerable<ContentTypeSnippetModel> managementSnippets,
+            ContentTypeModel managementContentType)
+        {
+            if (deliverElement == null)
+            {
+                throw new ArgumentNullException(nameof(deliverElement));
+            }
+
+            if (managementSnippets == null)
+            {
+                throw new ArgumentNullException(nameof(managementSnippets));
+            }
+
+            if (managementContentType == null)
+            {
+                throw new ArgumentNullException(nameof(managementContentType));
+            }
+        }
+    }
+}
