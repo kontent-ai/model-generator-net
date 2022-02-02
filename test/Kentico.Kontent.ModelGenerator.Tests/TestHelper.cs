f09d169d9f1146747dc2f1c03e9aaa6ae2d56dbf
Martin Ševčík <sevcik.martin4@gmail.com>
2022-02-02 15:07:20 +0100

    124 introduce testhelper

---
+++ test/Kentico.Kontent.ModelGenerator.Tests/TestHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Kentico.Kontent.Management.Models.Types.Elements;
+using Newtonsoft.Json.Linq;
+
+namespace Kentico.Kontent.ModelGenerator.Tests
+{
+    internal static class TestHelper
+    {
+        public static ElementMetadataBase GenerateElementMetadataBase(Guid elementId, string elementCodename, ElementMetadataType type = ElementMetadataType.Text) =>
+            JObject.FromObject(new
+            {
+                Id = elementId,
+                Codename = elementCodename,
+                type
+            }).ToObject<ElementMetadataBase>();
+    }
+}
