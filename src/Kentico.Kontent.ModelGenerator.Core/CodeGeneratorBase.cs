d86b574dc7419b83a054bc861206a54a6dd6c0fa
Martin Ševčík <sevcik.martin4@gmail.com>
2022-02-03 11:51:53 +0100

    124 reorder props

--- src/Kentico.Kontent.ModelGenerator.Core/CodeGeneratorBase.cs
+++ src/Kentico.Kontent.ModelGenerator.Core/CodeGeneratorBase.cs
@@ -10,9 +10,9 @@ namespace Kentico.Kontent.ModelGenerator.Core
         protected readonly CodeGeneratorOptions Options;
         protected readonly IOutputProvider OutputProvider;
 
+        protected string FilenameSuffix => string.IsNullOrEmpty(Options.FileNameSuffix) ? "" : $".{Options.FileNameSuffix}";
         protected string NoContentTypeAvailableMessage =>
             $@"No content type available for the project ({ProjectId}). Please make sure you have the Delivery API enabled at https://app.kontent.ai/.";
-        protected string FilenameSuffix => string.IsNullOrEmpty(Options.FileNameSuffix) ? "" : $".{Options.FileNameSuffix}";
 
         protected CodeGeneratorBase(IOptions<CodeGeneratorOptions> options, IOutputProvider outputProvider)
         {
