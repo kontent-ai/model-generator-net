62b5d9a823dac38052e1520eac1f96140bc3a154
Jono R <work@jrkd.co.uk>
2022-04-05 15:03:56 +1200

    Made amends based on PR feedback.  - Updated const variable names to camel case  - Added SyntaxHelpers class method SyntaxHelpers.GenerateComment(string)

---
+++ src/Kentico.Kontent.ModelGenerator.Core/SyntaxHelpers.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Kentico.Kontent.ModelGenerator.Core
+{
+    public static class SyntaxHelpers
+    {
+        public static SyntaxTrivia GenerateComment(string comment)
+        {
+            return SyntaxFactory.Comment(comment + Environment.NewLine + Environment.NewLine);
+        }
+    }
+}
