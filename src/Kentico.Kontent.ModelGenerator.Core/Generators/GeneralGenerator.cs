9a18bb5e73fafe80fa1eb19252b2d4b98fbb3f2e
Martin Ševčík <sevcik.martin4@gmail.com>
2022-04-14 10:13:04 +0200

    141 refactorings, introduce comments and autogenerated tag

---
+++ src/Kentico.Kontent.ModelGenerator.Core/Generators/GeneralGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Kentico.Kontent.ModelGenerator.Core.Generators.Class;
+using Microsoft.CodeAnalysis;
+
+namespace Kentico.Kontent.ModelGenerator.Core.Generators
+{
+    public abstract class GeneralGenerator
+    {
+        public readonly string Namespace;
+
+        protected GeneralGenerator(string @namespace = ClassCodeGenerator.DefaultNamespace)
+        {
+            Namespace = string.IsNullOrWhiteSpace(@namespace) ? ClassCodeGenerator.DefaultNamespace : @namespace;
+        }
+
+        protected abstract SyntaxTrivia ClassDescription();
+    }
+}
