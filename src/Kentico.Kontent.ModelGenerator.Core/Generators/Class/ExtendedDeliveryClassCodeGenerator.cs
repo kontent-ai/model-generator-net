37c5a025bcd1202ddaa2ccff16c22dd43608d026
Martin Ševčík <sevcik.martin4@gmail.com>
2022-05-12 14:38:22 +0200

    90 introduce ExtendedDeliveryClassCodeGenerator

---
+++ src/Kentico.Kontent.ModelGenerator.Core/Generators/Class/ExtendedDeliveryClassCodeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Kentico.Kontent.ModelGenerator.Core.Common;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Kentico.Kontent.ModelGenerator.Core.Generators.Class
+{
+    public class ExtendedDeliveryClassCodeGenerator : DeliveryClassCodeGenerator
+    {
+        private readonly string BaseTypeName;
+
+        public ExtendedDeliveryClassCodeGenerator(ClassDefinition classDefinition, string classFilename, string baseTypeName, string @namespace = DefaultNamespace)
+            : base(classDefinition, classFilename, @namespace)
+        {
+            BaseTypeName = baseTypeName;
+        }
+
+        private ExtendedDeliveryClassCodeGenerator(ClassDefinition classDefinition, string classFilename, string @namespace = DefaultNamespace)
+            : base(classDefinition, classFilename, @namespace)
+        {
+        }
+
+        protected override ClassDeclarationSyntax GetClassDeclaration()
+        {
+            var classDeclaration = base.GetClassDeclaration();
+
+            var baseType = SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName(BaseTypeName));
+            classDeclaration = classDeclaration.AddBaseListTypes(baseType);
+
+            return classDeclaration;
+        }
+    }
+}
