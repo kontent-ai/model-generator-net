9a8e6c6421ea527c72fc972d604c01f725c84331
Martin Ševčík <sevcik.martin4@gmail.com>
2022-05-26 13:03:37 +0200

    90 introduce ContentItemClassCodeGeneratorTests

---
+++ test/Kentico.Kontent.ModelGenerator.Tests/Generators/Class/ContentItemClassCodeGeneratorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Kentico.Kontent.ModelGenerator.Core.Generators.Class;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace Kentico.Kontent.ModelGenerator.Tests.Generators.Class
+{
+    public class ContentItemClassCodeGeneratorTests : TestBaseClassCodeGenerator
+    {
+        [Fact]
+        public void Constructor_CreatesInstance()
+        {
+            var classCodeGenerator = new ContentItemClassCodeGenerator();
+
+            Assert.NotNull(classCodeGenerator);
+            Assert.True(classCodeGenerator.OverwriteExisting);
+        }
+
+        [Fact]
+        public void Build_CreatesClassWithSystemProperty()
+        {
+            var classCodeGenerator = new ContentItemClassCodeGenerator();
+
+            var compiledCode = classCodeGenerator.GenerateCode();
+
+            var executingPath = AppContext.BaseDirectory;
+            var expectedCode = File.ReadAllText(executingPath + "/Assets/DefaultContentItem_CompiledCode.txt");
+
+            Assert.Equal(expectedCode, compiledCode, ignoreWhiteSpaceDifferences: true, ignoreLineEndingDifferences: true);
+        }
+
+        [Fact]
+        public void IntegrationTest_GeneratedCodeCompilesWithoutErrors()
+        {
+            var classCodeGenerator = new ContentItemClassCodeGenerator();
+
+            var compiledCode = classCodeGenerator.GenerateCode();
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName: Path.GetRandomFileName(),
+                syntaxTrees: new[] { CSharpSyntaxTree.ParseText(compiledCode) },
+                references: new[] {
+                    MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(Delivery.Abstractions.IApiResponse).GetTypeInfo().Assembly.Location)
+                },
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            AssertCompiledCode(compilation);
+        }
+    }
+}
