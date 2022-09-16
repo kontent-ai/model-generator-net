a9903daab80d10baa10eae0aad0ad9e6980b9937
Martin Ševčík <sevcik.martin4@gmail.com>
2022-09-16 16:03:23 +0200

    90 introduce class code generators

---
+++ src/Kontent.Ai.ModelGenerator.Core/Generators/Class/ContentItemClassCodeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Kontent.Ai.ModelGenerator.Core.Common;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Kontent.Ai.ModelGenerator.Core.Generators.Class;
+
+public class ContentItemClassCodeGenerator : ClassCodeGenerator
+{
+    public const string DefaultContentItemClassName = "IContentItem";
+
+    public ContentItemClassCodeGenerator(string @namespace = DefaultNamespace)
+        : this(new ClassDefinition(DefaultContentItemClassName), DefaultContentItemClassName, @namespace)
+    {
+    }
+
+    private ContentItemClassCodeGenerator(ClassDefinition classDefinition, string classFilename, string @namespace = DefaultNamespace)
+        : base(classDefinition, classFilename, @namespace)
+    {
+    }
+
+    protected override UsingDirectiveSyntax[] GetApiUsings()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Kontent.Ai.ModelGenerator.Core/Generators/Class/TypedExtendedDeliveryClassCodeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Kontent.Ai.ModelGenerator.Core.Common;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Kontent.Ai.ModelGenerator.Core.Generators.Class;
+
+public class TypedExtendedDeliveryClassCodeGenerator : ClassCodeGenerator
+{
+    public TypedExtendedDeliveryClassCodeGenerator(ClassDefinition classDefinition, string classFilename, string @namespace = DefaultNamespace)
+        : base(classDefinition, classFilename, @namespace)
+    {
+    }
+
+    protected override UsingDirectiveSyntax[] GetApiUsings()
+    {
+        throw new NotImplementedException();
+    }
+}
