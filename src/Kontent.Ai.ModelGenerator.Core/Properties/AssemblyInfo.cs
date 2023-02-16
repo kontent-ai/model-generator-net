57887895477543e51f0bfddcd574b99cdd027dab
Martin Ševčík <sevcik.martin4@gmail.com>
2023-02-16 10:23:43 +0100

    158 track usage of generators sdk

--- src/Kontent.Ai.ModelGenerator.Core/Properties/AssemblyInfo.cs
+++ src/Kontent.Ai.ModelGenerator.Core/Properties/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Kontent.Ai.Management.Attributes;
+using Kontent.Ai.Delivery;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Kontent.Ai.ModelGenerator.Tests")]
+[assembly: SourceTrackingHeaderAttribute]
+[assembly: DeliverySourceTrackingHeader]
