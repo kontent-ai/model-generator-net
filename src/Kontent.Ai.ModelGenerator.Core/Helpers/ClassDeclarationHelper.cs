ebe7080c1aa7d5b5761baaf2a34561843d9ec38f
Martin Ševčík <sevcik.martin4@gmail.com>
2023-04-20 16:46:11 +0200

    168 introduce tests for ClassDeclarationHelper

--- src/Kontent.Ai.ModelGenerator.Core/Helpers/ClassDeclarationHelper.cs
+++ src/Kontent.Ai.ModelGenerator.Core/Helpers/ClassDeclarationHelper.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
 namespace Kontent.Ai.ModelGenerator.Core.Helpers;
 
 internal static class ClassDeclarationHelper
 {
-    public static SyntaxTrivia GenerateSyntaxTrivia(string customComment) => SyntaxFactory.Comment(TextHelpers.GenerateCommentString(customComment));
+    public static SyntaxTrivia GenerateSyntaxTrivia(string customComment)
+    {
+        if (string.IsNullOrWhiteSpace(customComment))
+        {
+            throw new ArgumentNullException(nameof(customComment));
+        }
+
+        if (!customComment.StartsWith("// "))
+        {
+            throw new ArgumentException("Comment is invalid.", nameof(customComment));
+        }
+
+        return SyntaxFactory.Comment(TextHelpers.GenerateCommentString(customComment));
+    }
 }
