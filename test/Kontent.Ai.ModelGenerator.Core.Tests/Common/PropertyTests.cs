6ac155b2521cb125196d154714a361ebcff539b6
Daniel Pokorny <52500882+pokornyd@users.noreply.github.com>
2026-03-03 17:11:12 +0100

    make all model props nullable

--- test/Kontent.Ai.ModelGenerator.Core.Tests/Common/PropertyTests.cs
+++ test/Kontent.Ai.ModelGenerator.Core.Tests/Common/PropertyTests.cs
@@ -31,15 +31,15 @@ public class PropertyTests
     }
 
     [Theory]
-    [InlineData("text", "string")]
-    [InlineData("rich_text", "RichTextContent")]
+    [InlineData("text", "string?")]
+    [InlineData("rich_text", "RichTextContent?")]
     [InlineData("number", "double?")]
-    [InlineData("multiple_choice", "IEnumerable<MultipleChoiceOption>")]
-    [InlineData("date_time", "DateTimeContent")]
-    [InlineData("asset", "IEnumerable<Asset>")]
-    [InlineData("modular_content", "IEnumerable<IEmbeddedContent>")]
-    [InlineData("taxonomy", "IEnumerable<TaxonomyTerm>")]
-    [InlineData("url_slug", "string")]
+    [InlineData("multiple_choice", "IEnumerable<MultipleChoiceOption>?")]
+    [InlineData("date_time", "DateTimeContent?")]
+    [InlineData("asset", "IEnumerable<Asset>?")]
+    [InlineData("modular_content", "IEnumerable<IEmbeddedContent>?")]
+    [InlineData("taxonomy", "IEnumerable<TaxonomyTerm>?")]
+    [InlineData("url_slug", "string?")]
     [InlineData("custom", "string?")]
     public void FromContentTypeElement_DeliveryApiModel_Returns(string contentType, string expectedTypeName)
     {
